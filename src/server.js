f799a8ac8a0c609034b7fc8a0d2369d1ed927b8d
dolanap <gyrms9412@naver.com>
2022-03-10 21:19:15 +0900

    create Router & Controller - add basic Router - add test Controller

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import express from "express"; //no babel: const express = require("express");
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 const app = express();
 
-//Application Setting
-const handleHome = (req, res) => {
-  console.log("Somebody is trying to go home..");
-  return res.send("This is response ✔");
-};
+const logger = morgan("dev"); //Middleware for log
+app.use(logger);
 
-app.get("/", handleHome);
+//Router
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on localhost:${PORT}✨`);
