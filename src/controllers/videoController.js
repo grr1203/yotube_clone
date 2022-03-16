1960c840687defd8d7972de985863463f52c4fb2
dolanap <gyrms9412@naver.com>
2022-03-16 20:25:42 +0900

    Add & Update Template - redirect views directory path - create mixin file: video.pug - add new middleware for req.body - add fake DB - add temporary CSS(MVP.css)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,38 @@
-export const trending = (req, res) => res.render("home");
+const fakeUser = {
+  username: "Hyo",
+  loggedIn: false,
+};
+
+let videos = [
+  { title: "First Video", rating: 5, views: 1, id: 1 },
+  { title: "Second Video", rating: 4, views: 59, id: 2 },
+  { title: "Third Video", rating: 3, views: 59, id: 3 },
+];
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser, videos });
+
 export const search = (req, res) => res.send("Search Videos");
+
 export const upload = (req, res) => res.send("Upload");
-export const see = (req, res) => res.send(`See Video${req.params.id}`);
-export const edit = (req, res) => res.send("Edit Video");
+
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watch ${video.title}`, video });
+};
+
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  res.redirect(`/video/${id}`);
+};
+
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -8,9 +8,11 @@ const PORT = 4000;
 const app = express();
 
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 
 const logger = morgan("dev"); //Middleware for log
 app.use(logger);
+app.use(express.urlencoded({ extended: true })); //for using req.body
 
 //Router
 app.use("/", globalRouter);
