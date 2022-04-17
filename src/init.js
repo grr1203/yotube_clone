1f08e00bcd79f34bd0fab7869d1bef0c7d891b1a
dolanap <gyrms9412@naver.com>
2022-04-17 19:04:43 +0900

    Deployment Using Heroku

--- src/init.js
+++ src/init.js
@@ -1,13 +1,13 @@
 import "dotenv/config";
+import "regenerator-runtime";
 import "./db";
 import "./models/Video";
 import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000; //heroku or local
 
-const handleListening = () =>
-  console.log(`Server listening on localhost:${PORT}✨`);
+const handleListening = () => console.log(`Server listening on PORT:${PORT}✨`);
 
 app.listen(PORT, handleListening);
